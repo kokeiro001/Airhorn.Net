3b31d917e316b86ee1b94b63abff0deb7e952b04
kokeiro <kokeiro.ssbm.001@gmail.com>
2017-06-25 23:19:47 +0900

    プロジェクト ファイルを追加します。

---
+++ AirhornNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AirhornNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
